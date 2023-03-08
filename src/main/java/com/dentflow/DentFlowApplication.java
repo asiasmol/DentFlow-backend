5b1237763c18173ca03345e8369562f22342a823
Tomasz Kraczek <tomek6363@gmail.com>
2023-03-08 14:56:35 +0100

    Initial commit.

---
+++ src/main/java/com/dentflow/DentFlowApplication.java
@@ -0,0 +1,13 @@
+package com.dentflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dentflow/DentFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
