fa87432c45b8a1db162b993790e7b2f416dfb0d6
Psort <skowronpiotr.00@gmail.com>
2023-03-24 15:02:40 +0100

    Add Security and mail sennder

---
+++ src/main/java/com/dentflow/security/EmailService.java
@@ -0,0 +1,21 @@
+package com.dentflow.security;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private JavaMailSender javaMailSender;
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage mail = new SimpleMailMessage();
+        mail.setTo(to);
+        mail.setSubject(subject);
+        mail.setText(body);
+        javaMailSender.send(mail);
+    }
+}
+
