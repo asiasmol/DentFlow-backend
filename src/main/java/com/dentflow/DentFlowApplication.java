88da365d952ec0502cb4fc678dfed1c504b55664
Tomasz Kraczek <tomek6363@gmail.com>
2023-03-24 15:23:15 +0100

    Fix. security temporary disabled.

--- src/main/java/com/dentflow/DentFlowApplication.java
+++ src/main/java/com/dentflow/DentFlowApplication.java
@@ -3,14 +3,14 @@ package com.dentflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class DentFlowApplication {
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+//	@Bean
+//	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
 
 
 	public static void main(String[] args) {
--- src/main/java/com/dentflow/security/WebSecurityConfig.java
+++ src/main/java/com/dentflow/security/WebSecurityConfig.java
@@ -1,32 +1,32 @@
-package com.dentflow.security;
-
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/**").permitAll()
-                )
-                .formLogin((form) -> form
-                .loginPage("/login")
-                .permitAll())
-                .logout((logout) -> logout.permitAll()
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/")
-                        .invalidateHttpSession(true)
-                        .deleteCookies("JSESSIONID"));
-        return http.build();
-    }
-
-
-
-}
+//package com.dentflow.security;
+//
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig {
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((requests) -> requests
+//                        .requestMatchers("/**", "/api/**").permitAll()
+//                )
+//                .formLogin((form) -> form
+//                .loginPage("/login")
+//                .permitAll())
+//                .logout((logout) -> logout.permitAll()
+//                        .logoutUrl("/logout")
+//                        .logoutSuccessUrl("/")
+//                        .invalidateHttpSession(true)
+//                        .deleteCookies("JSESSIONID"));
+//        return http.build();
+//    }
+//
+//
+//
+//}
