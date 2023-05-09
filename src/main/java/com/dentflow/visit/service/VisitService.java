c75aa7b06a892f53ea3214f6455dd1b0726de42a
PrzemekKomorowski <106514184+PrzemekKomorowski@users.noreply.github.com>
2023-05-09 15:46:17 +0200

    editing visit description

--- src/main/java/com/dentflow/visit/service/VisitService.java
+++ src/main/java/com/dentflow/visit/service/VisitService.java
@@ -49,4 +49,12 @@ public class VisitService {
         clinicRepository.save(clinic);
     }
 
+    public void saveDescriptionToVisit(VisitRequest visitRequest, String email){
+        Clinic clinic = userService.getUser(email).getClinics().stream().filter(c -> Objects.equals(c.getId(), visitRequest.getClinicId())).findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Clinic not found"));
+        Visit visit = clinic.getVisits().stream().filter(v -> Objects.equals(v.getId(), visitRequest.getVisitId())).findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Clinic not found"));
+        visit.setDescription(visitRequest.getDescription());
+        visitRepository.save(visit);
+    }
 }
