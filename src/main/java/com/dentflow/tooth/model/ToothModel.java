1e07892a8edb32734314dfcabb966034740b977d
Psort <skowronpiotr.00@gmail.com>
2023-05-10 10:08:08 +0200

    add tooth model

---
+++ src/main/java/com/dentflow/tooth/model/ToothModel.java
@@ -0,0 +1,30 @@
+package com.dentflow.tooth.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ToothModel {
+    private int number;
+    private Boolean forObservation;
+    private Boolean caries;
+    private Boolean secondaryCaries;
+    private Boolean filling;
+    private Boolean prostheticCrown;
+    private Boolean channelsFilledCorrectly;
+    private Boolean channelNotCompleted;
+    private Boolean periapicalChange;
+    private Boolean crownRootInsert;
+    private Boolean supragingivalCalculus;
+    private Boolean subgingivalCalculus;
+    private Boolean impactedTooth;
+    private Boolean noTooth;
+    private Boolean microdonticTooth;
+    private Boolean developmentalDefect;
+    private Boolean pathologicalClash;
+}
